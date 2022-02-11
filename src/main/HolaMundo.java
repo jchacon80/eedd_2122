dbffece8972b99df1ac3ddfd6309b177b705ed38
jorge <jchacon80@gmail.com>
2022-02-11 13:42:12 +0100

    segundo commit desde eclipse

--- src/main/HolaMundo.java
+++ src/main/HolaMundo.java
@@ -3,7 +3,7 @@ package main;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// Esto es una prueba
+		// Esto es una prueba de un conflicto
 		System.out.println("hola mundo");
 
 	}
